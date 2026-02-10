9c6ca00e9399cd1c9a3228ca483f06270288d8be
unknown <cse.tahiyasultana.ms@gmail.com>
2026-02-10 21:34:42 +1100

    Add Home Page with topic options

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,26 @@
+const topics = [
+  { key: "linux", label: "Linux 🐧" },
+  { key: "cpp", label: "C++ 💻" },
+];
+
+function Home({ onSelectTopic }) {
+  return (
+    <div className="home">
+      <h1>WiCS Quiz Game</h1>
+      <p>Select a topic to begin</p>
+
+      <div className="topic-list">
+        {topics.map((topic) => (
+          <button
+            key={topic.key}
+            className="topic-button"
+          >
+            {topic.label}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
