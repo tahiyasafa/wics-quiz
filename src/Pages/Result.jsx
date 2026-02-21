1bd3c7bd99d930df641a4e8bf37e390b8e51905f
unknown <cse.tahiyasultana.ms@gmail.com>
2026-02-21 16:54:40 +1100

    Modify Result Page

--- src/Pages/Result.jsx
+++ src/Pages/Result.jsx
@@ -2,19 +2,22 @@
 function Result({ score, total }) {
   return (
     <div className="result">
-      <h1>WiCS Quiz Game</h1>
 
       <div>
-        <h2>Your Score</h2>
-        <p>{score} / {total}</p>
+        <h1 style={{ color: '#491661', fontSize: '2rem' }}>Your Score</h1>
+        <h2>{score} / {total}</h2>
         {score >= 6 ? (
         <>
         <h4>You get a cookie!</h4>
         <img src="cookies.png" className="cookie" />
       </>
       ) : (
+        <>
         <h4>Better luck next time.</h4>
+        <img src="sad.png" className="cookie" />
+        </>
       )}
+      <button onClick={() => window.location.reload()}>Try Again</button>
       </div>
     </div>
   );
